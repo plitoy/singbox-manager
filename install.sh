22ff61a89c66e9a1fc809cad7ac7fb5db6ad1e4a
hynize <local@release>
2026-09-22 13:22:13 +0800

    v1.5.8: 重烙 tarball/install.sh 摘要链(修复后 tarball: 68c3027d…af90→feaf3e08…507a)

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ REPO_NAME="singbox-manager"
 PROJECT_VERSION="v1.5.8"
 PACKAGE_NAME="singbox-manager-v1.5.8.tar.gz"
 # 发布流程：scripts/build-release-bundle.sh 构建可复现 bundle，其 SHA256 与此处一致
-PACKAGE_SHA256="96994fc873394afe7a50f485c070d3f86b99193fa2d9412f76471952a1c9343c"
+PACKAGE_SHA256="68c3027d1e593882265afddcd3f99971eb35d99c513a4ced1cc3ea05a799af90"
 PACKAGE_URL="https://github.com/${REPO_OWNER}/${REPO_NAME}/releases/download/${PROJECT_VERSION}/${PACKAGE_NAME}"
 
 INSTALL_BIN="/usr/local/bin/sbm"
